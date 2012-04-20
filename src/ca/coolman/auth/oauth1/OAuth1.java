f09dc33ab9bb740f0c40cc67d1c5a4db0a0317d5
1815750 <1815750@coolman.ca>
2012-04-20 02:17:45 +0000

    Added an oauth credentials registry to allow initializing OAuth for multiple API endpoints in application initialize lifecycle.  Removed constructor that accepted OAuth consumer key and secret. Added a signRequest() method for signing API requests with access token.

--- src/ca/coolman/auth/oauth1/OAuth1.java
+++ src/ca/coolman/auth/oauth1/OAuth1.java
@@ -24,36 +24,73 @@
  */
 package ca.coolman.auth.oauth1;
 
+import java.util.Hashtable;
+
 import ca.coolman.components.ObservableWebBrowser;
 
 import com.codename1.components.WebBrowser;
+import com.codename1.io.ConnectionRequest;
 import com.codename1.io.NetworkManager;
 import com.codename1.io.Storage;
 import com.codename1.io.Util;
+import com.codename1.processing.Result;
 
 /**
+ * The OAuth1 class implements the OAuth flow, as documented here, for example:
+ * 
+ * <pre>
+ * https://dev.twitter.com/docs/auth/implementing-sign-twitter
+ * </pre>
  * 
  * @author Eric Coolman
- *
+ * 
  */
 public class OAuth1 {
 	private final static String URL_AUTHENTICATE = Request.BASEURL
 			+ "authenticate";
 
+	private static Hashtable credentials;
 	private static SigningImplementation _signerImplementation = null;
 	private Signer signer;
 	private String callback;
 
-	public OAuth1(String consumerKey, String consumerSecret, String callback) {
+	public OAuth1(String callback) {
 		if (_signerImplementation == null) {
 			throw new IllegalStateException(
 					"Signer implementation has not been initialized!");
+		}
+		if (credentials == null) {
+			throw new IllegalStateException(
+				"Consumer credentials has not been initialized!");
+		}
+		Result result = Result.fromContent(credentials);
+		String appId = callback.replace('/','_');
+		String consumerKey = result.getAsString('/' + appId + "/key");
+		String consumerSecret = result.getAsString('/' + appId + "/secret");
+		if ((consumerKey == null) || (consumerSecret == null)) {
+			throw new IllegalStateException(
+					"Consumer credentials has not been initialized for callback!");
 		}
 		this.signer = new Signer(_signerImplementation, consumerKey,
 				consumerSecret);
 		this.callback = callback;
 	}
 
+	public void signRequest(ConnectionRequest request, AccessToken token) {
+		signer.sign(request, token);
+	}
+
+	public static void register(String callback, final String consumerKey,
+			final String consumerSecret) {
+		if (credentials == null) {
+			credentials = new Hashtable();
+		}
+		Hashtable credential = new Hashtable();
+		credential.put("key", consumerKey);
+		credential.put("secret", consumerSecret);
+		credentials.put(callback.replace('/', '_'), credential);
+	}
+	
 	public static void initialize(SigningImplementation impl) {
 		_signerImplementation = impl;
 		Util.register(AccessToken.OBJECT_ID, AccessToken.class);
