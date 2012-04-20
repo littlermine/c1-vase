d7b2b1d2969c776e92411417c50ef909ca2c8ec0
1815750 <1815750@coolman.ca>
2012-04-20 02:40:19 +0000

    Initial Checkin

---
+++ src/ca/coolman/social/twitter/TwitterService.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2012, Eric Coolman, 1815750 Ontario Inc. and/or its 
+ * affiliates. All rights reserved.
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  1815750 Ontario Inc designates 
+ * this  * particular file as subject to the "Classpath" exception as provided
+ * in the LICENSE file that accompanied this code.
+ *  
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * 
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Please contact 1815750 Ontario Inc. through http://www.coolman.ca/ if you 
+ * need additional information or have any questions.
+ */
+package ca.coolman.social.twitter;
+
+import com.codename1.io.services.TwitterRESTService;
+
+/**
+ * @author Eric Coolman
+ * 
+ */
+public class TwitterService extends TwitterRESTService {
+	//private static final String CALLBACK = "http://www.themmaapp.com/sign-in-with-twitter";
+	private boolean rateLimited;
+	private boolean requiresAuth;
+
+	public TwitterService(String method, String version, boolean post,
+			boolean requiresAuth, boolean rateLimited) {
+		super(method, version, post);
+		this.rateLimited = rateLimited;
+		this.requiresAuth = requiresAuth;
+	}
+
+	/**
+	 * @return the rateLimited
+	 */
+	public boolean isRateLimited() {
+		return rateLimited;
+	}
+
+	/**
+	 * @return the requiresAuth
+	 */
+	public boolean isRequiresAuth() {
+		return requiresAuth;
+	}
+
+}
