bd1d66a3cb80644eda1febab40cd97b9dc6411e2
1815750 <1815750@coolman.ca>
2012-04-29 20:10:49 +0000

    added new authorize url as per new ServiceProvider contract, and updated for change to the getAuthenticateUrl() implementation, which now passes a RequestToken

--- src/ca/coolman/social/twitter/TwitterServiceProvider.java
+++ src/ca/coolman/social/twitter/TwitterServiceProvider.java
@@ -1,9 +1,31 @@
-/**
+/*
+ * Copyright (c) 2012, Eric Coolman, 1815750 Ontario Inc. and/or its 
+ * affiliates. All rights reserved.
  * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  1815750 Ontario Inc designates 
+ * this  * particular file as subject to the "Classpath" exception as provided
+ * in the LICENSE file that accompanied this code.
+ *  
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * 
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Please contact 1815750 Ontario Inc. through http://www.coolman.ca/ if you 
+ * need additional information or have any questions.
  */
 package ca.coolman.social.twitter;
 
 import ca.coolman.auth.oauth1.HmacSha1;
+import ca.coolman.auth.oauth1.RequestToken;
 import ca.coolman.auth.oauth1.ServiceProvider;
 
 /**
@@ -32,6 +54,13 @@ public class TwitterServiceProvider extends ServiceProvider {
 	private TwitterServiceProvider(String id, String protocol) {
 		super((id == null) ? "twitter" : id, protocol + BASEURL
 				+ "request_token", protocol + BASEURL + "access_token",
-				protocol + BASEURL + "authenticate", new HmacSha1());
+				protocol + BASEURL + "authenticate", protocol + BASEURL + "authenticate", new HmacSha1());
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.coolman.auth.oauth1.ServiceProvider#getAuthenticateUrl(ca.coolman.auth.oauth1.RequestToken)
+	 */
+	public String getAuthenticateUrl(RequestToken requestToken) {
+		return super.getAuthenticateUrl(requestToken) + "&force_login=true";
 	}
 }
