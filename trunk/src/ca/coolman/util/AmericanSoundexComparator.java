2d3727659c299c4c968921ae8da5275eba212a52
1815750@coolman.ca <1815750@coolman.ca@f3ff68f1-e500-9c3d-7780-0a0f82f52178>
2012-04-29 20:12:36 +0000

    removed static main methods, it is causing the IOS builds to fail.  Moved them to new test/ area.

--- trunk/src/ca/coolman/util/AmericanSoundexComparator.java
+++ trunk/src/ca/coolman/util/AmericanSoundexComparator.java
@@ -118,18 +118,4 @@ public class AmericanSoundexComparator extends SoundexComparator {
 		return (stripVowels(result) + "000").substring(0, 4);
 	}
 
-	/**
-	 * A method to test the names described on the wikipedia article.
-	 * 
-	 * @param args
-	 */
-	public static void main(String args[]) {
-		String test[] = {"Robert", "Rupert", "Rubin", "Ashcraft", "Ashcroft",
-				"Tymczak", "Pfister"};
-		for (int i = 0; i < test.length; i++) {
-			System.out.println(test[i] + " = "
-					+ new AmericanSoundexComparator().getSoundex(test[i]));
-		}
-	}
-
 }
